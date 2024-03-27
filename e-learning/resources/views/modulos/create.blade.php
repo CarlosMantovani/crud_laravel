216e39e98c488eb017089489c1c58484a3732fe6
Carlos Henrique Mantovani <111471011+CarlosMantovani@users.noreply.github.com>
2024-03-27 19:47:08 -0300

    Update create.blade.php

--- e-learning/resources/views/modulos/create.blade.php
+++ e-learning/resources/views/modulos/create.blade.php
@@ -7,4 +7,20 @@
             @csrf
             <div class="col">
                 <label for="inputAddress" class="form-label">Título do Módulo</label>
-                <input type="text"
+                <input type="text" class="form-control" id="inputAddress" name="titulo" required>
+            </div>
+            <div class="col">
+                <label for="curso_id" class="form-label">Escolha o Curso</label>
+                <select id="curso_id" name="curso_id" class="form-control" required>
+                    @foreach ($cursos as $curso)
+                        <option value="{{ $curso->id }}">{{ $curso->nome_curso }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+        </form>
+        <a href="{{ route('modulos.index') }}" class="btn btn-primary mt-2">Voltar</a>
+    </div>
+@endsection
